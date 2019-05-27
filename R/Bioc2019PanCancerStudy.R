60e66e43d858022a2d5830296b194890b0ab0700
Gabriel J. Odom <gjo15@med.miami.edu>
2019-05-27 10:30:34 -0400

    Per Issue #1, split off data documentation

--- R/Bioc2019PanCancerStudy.R
+++ R/Bioc2019PanCancerStudy.R
@@ -7,17 +7,3 @@
 #' @docType package
 #' @name Bioc2019PanCancerStudy
 NULL
-
-#' A numeric vector with Stemness signature trained on PCBC's dataset
-#' @docType data
-#' @keywords internal
-#' @name PCBC_stemSig
-#' @format A numeric vector with 12955 genes
-NULL
-
-#' A matrix with Immune subtypes trained on TCGA's dataset
-#' @docType data
-#' @keywords internal
-#' @name ImmuneMW
-#' @format A matrix with 13742 genes and 6 immune subtypes
-NULL
---
+++ R/data_Bioc2019PanCancerStudy.R
@@ -0,0 +1,14 @@
+
+#' A numeric vector with Stemness signature trained on PCBC's dataset
+#' @docType data
+#' @keywords internal
+#' @name PCBC_stemSig
+#' @format A numeric vector with 12955 genes
+"PCBC_stemSig"
+
+#' A matrix with Immune subtypes trained on TCGA's dataset
+#' @docType data
+#' @keywords internal
+#' @name ImmuneMW
+#' @format A matrix with 13742 genes and 6 immune subtypes
+"ImmuneMW"
