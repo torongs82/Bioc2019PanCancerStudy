0619252d04d214e227faece7958cf193299913cc
Antonio Colaprico <antonio.colaprico@gmail.com>
2019-05-27 10:53:39 -0400

    documentation for BRCARnaseqSE, TableDataTypes
    documentation for BRCARnaseqSE, TableDataTypes

--- R/data_Bioc2019PanCancerStudy.R
+++ R/data_Bioc2019PanCancerStudy.R
@@ -12,3 +12,17 @@
 #' @name ImmuneMW
 #' @format A matrix with 13742 genes and 6 immune subtypes
 "ImmuneMW"
+
+#' A data.frame for TCGAbiolinks to falitate multi -omics data retrieval
+#' @docType data
+#' @keywords internal
+#' @name TableDataTypes
+#' @format A data.frame with 7 rows (datatypes) and 4 columns (parameters)
+"TableDataTypes"
+
+#' A SummarizedExperiment object with 20 TCGA BRCA samples obtained from TCGAbiolinks
+#' @docType data
+#' @keywords internal
+#' @name BRCARnaseqSE
+#' @format A SummarizedExperiment with 21022 genes and 20 samples
+"BRCARnaseqSE"
